5cc10e056bf65d6766edcdcd2895373278cd4a55
TSYMarina <marinatsy16@gmail.com>
2022-03-07 13:21:35 -0700

    first commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,78 @@
 using System;
 using Xunit;
+using UnitTestingExercise;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3, 6)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(-10, -2, 5, -7)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var addTest = new UnitTestMethods();
             //Act
-
+            var actual = addTest.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,2,8)]
+        [InlineData(-8, -2, -10)]
+        [InlineData(0, 0, 0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var substTest = new UnitTestMethods();
             //Act
-
+            var actual = substTest.Substract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 3, 6)]
+        [InlineData(-2, 3, -6)]
+        [InlineData(0, 0, 0)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var multTest = new UnitTestMethods();
             //Act
-
+            var actual = multTest.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,5,2)]
+        [InlineData(-10, 5, -2)]
+        [InlineData(0, 0, 0)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var divTest = new UnitTestMethods();
             //Act
-
+            var actual = divTest.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
+
 
         [Fact]
-        public void YourMethodName()
+        public void GetGreatDay()
         {
             //Arrange
-
+            var byeTest = new UnitTestMethods();
             //Act
-
+            var actual = "Have a great day!";
             //Assert
-
+            Assert.Matches("Have a great day!", actual);
         }
 
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,26 +1,54 @@
 ﻿using System;
+
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // DONE - Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int num1, int num2, int num3)
+        {
+            int sum = num1 + num2 + num3;
+            return sum;
+        }
+
+
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+
+        public int Substract(int minuend, int subtrhend)
+        {
+            int result = minuend - subtrhend;
+            return result;
+        }
 
         // Create a Multiply method that passes 2 integers
 
+        public int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
         // Create a Divide method that passes 2 integers
 
-
+        public int Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+        public string GetGreatDay()
+        {
+            return ("Have a great day!");
+        }
     }
 }
